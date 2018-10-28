0e5a0beb315dcaaa1f8ead2f49423bca6babeb83
Morten Borup Petersen <morten_bp@live.dk>
2018-10-28 11:07:14 +0100

    Format example

--- tests/c/triangle.c
+++ tests/c/triangle.c
@@ -1,1 +1,7 @@
-int triangle(int n) {int s = 0;for(int i = 1; i < n; i++){ s += i; }return s;}
+int triangle(int n) {
+    int s = 0;
+    for(int i = 1; i <= n; i++){
+        s += i; 
+    }
+    return s;
+}
