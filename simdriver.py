02fc4d33989470b33b118c3764da82ba2b3ba976
Morten Borup Petersen <morten_bp@live.dk>
2018-11-02 09:09:40 +0100

    Generalize simdriver.py, taking an input file for specifying test cases

--- simdriver.py
+++ simdriver.py
@@ -9,7 +9,16 @@ class DriverOptions:
     simExecutable = ""
     testPath = ""
 
+class testSpec:
+    testFunctor = ""
+    rangeStart = 0;
+    rangeEnd = 0;
+    interval = 0;
+    testFile = ""
+
 SPECIAL_REGISTERS = ["ACC", "ADDR", "PC", "INSTRUCTIONS EXECUTED"]
+
+# ------------ TEST FUNCTCIONS -------------------
 
 def triangleNumber(n):
     s = 0;
@@ -17,14 +26,37 @@ def triangleNumber(n):
         s += i
     return s;
 
+# ------------------------------------------------
+
 class Driver:
 
     options = []
     def __init__(self, options):
-        self.options = options;
+        self.options = options
+        self.scriptPath = os.path.dirname(os.path.realpath(__file__))
+        self.testSpecs = self.parseTestFile(options.testFilePath)
 
-        self.runTriangleTest()
+        for spec in self.testSpecs:
+            self.runTest(spec)
         return
+
+    def parseTestFile(self, testFilepath):
+        testSpecs = []
+        with open(testFilepath) as f:
+            c = f.readlines()
+            for line in c:
+                # Tokenize and remove newlines
+                line = line.rstrip()
+                tokens = line.split(';')
+                ts = testSpec()
+                ts.testFunctor = tokens[0]
+                ts.rangeStart = int(tokens[1])
+                ts.rangeEnd = int(tokens[2])
+                ts.interval = int(tokens[3])
+                ts.testFile = os.path.join(self.scriptPath, tokens[4])
+                testSpecs.append(ts)
+        return testSpecs
+
 
     def getTestNames(self, file):
         filename = os.path.splitext(file)[0]
@@ -47,13 +79,14 @@ class Driver:
                 registerStates[int(p[0])] = int(p[1])
         return registerStates
 
-    def runTriangleTest(self):
-        for i in range (0, 100):
+    def runTest(self, spec):
+        for i in range(spec.rangeStart, spec.rangeEnd, spec.interval):
             inputRegState = {}
             outputRegState = {}
             inputRegState[4] = i;
-            outputRegState[4] = triangleNumber(i)
-            self.execute(self.options.testPath, inputRegState, outputRegState)
+            outputRegState[4] = eval(spec.testFunctor + "(" + str(i) + ")")
+            self.execute(spec.testFile, inputRegState, outputRegState)
+
 
     def execute(self, testPath, inputRegState, expectedRegState):
         os.chdir(os.path.dirname(os.path.realpath(testPath)))
@@ -103,18 +136,19 @@ if __name__ == "__main__":
 
     parser.add_argument("--llp", help="Path to the LLVM tools which are to be used")
     parser.add_argument("--sim", help="Path to the simulator executable")
-    parser.add_argument("--test", help="Path to the test file")
+    parser.add_argument("--test", help="Path to the test file specification")
 
-    args = parser.parse_args().__dict__
+    args = parser.parse_args()
 
-    if len(args) == 3:
+    if len(sys.argv) > 3:
         opt = DriverOptions()
-        opt.llvmPath = os.path.expanduser(args["llp"])
-        opt.simExecutable = os.path.expanduser(args["sim"])
-        opt.testPath = os.path.expanduser(args["test"])
+        opt.llvmPath = os.path.expanduser(args.llp)
+        opt.simExecutable = os.path.expanduser(args.sim)
+        opt.testFilePath = os.path.expanduser(args.test)
 
         driver = Driver(opt)
 
         sys.exit(0)
 
+    parser.print_help()
     sys.exit(1)
