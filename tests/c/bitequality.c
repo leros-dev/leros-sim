10573f4a6b3cb265b91f29553a78866614965441
Morten Borup Petersen <morten_bp@live.dk>
2018-11-04 08:56:19 +0100

    Add bitequality tests

---
+++ tests/c/bitequality.c
@@ -0,0 +1,14 @@
+#define ACNT3
+#include "testmacro.h"
+
+TEST_START{ 
+    /* Move ARG(0)'th to rightmost side */
+    unsigned int bit1 =  (ARG(0) >> ARG(1)) & 1; 
+  
+    /* Move ARG(2)'th to rightmost side */
+    unsigned int bit2 =  (ARG(0) >> ARG(2)) & 1; 
+  
+    /* Equate the two bits */
+    unsigned int x = (bit1  == bit2); 
+    TEST_END(x); 
+} 
