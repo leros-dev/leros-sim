f7787e25b84abed8d45736e0105d3fa343da452c
Morten Borup Petersen <morten_bp@live.dk>
2018-11-04 10:18:03 +0100

    Add tests for constant boolean operations

---
+++ tests/c/boolean_op_1.c
@@ -0,0 +1,10 @@
+#define ACNT3
+#include "testmacro.h"
+
+TEST_START{
+    unsigned v1 = ARG(0) & 0b1;
+	unsigned v2 = ARG(1) | 0b1;
+	unsigned v3 = ARG(2) ^ 0b1;
+	unsigned v4 = v1 + v2 + v3;
+    TEST_END(v4);
+}
