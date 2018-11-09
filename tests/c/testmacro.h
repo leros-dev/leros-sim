c8e83e0165c232a7e836c55644fda6e3dd9db69a
Morten Borup Petersen <morten_bp@live.dk>
2018-11-09 15:07:18 +0100

    Modify testmacro for using main functions as test function for both host and simulator

--- tests/c/testmacro.h
+++ tests/c/testmacro.h
@@ -1,30 +1,59 @@
+#define ARG_PREFIX _A
+
 #ifdef LEROS_HOST_TEST
     #include <stdio.h>
     #include <string>
 
-    #define TEST_START int main(int argc, char** argv)
+    #define TEST_START int main(int argc, char** argv){
 
     #define TEST_END(retval) \
         printf("%d", retval); \
-        return 0;
+        return 0; \
+        }
 
 
     #define ARG(i) std::stoi(argv[i+1])
 
 #else   
-    // Argument count for the Leros function. Should be defined before including this file!
+
+    #define  TEST_END(retval)    \
+            return retval;       \
+            }
+
+    // We assume that argv for leros points to a position in memory where the arguments are passed
+    // as integers. This is mostly for the test - the Leros simulator will parse the input values as 
+    // integers and place them accordingly in memory.
+
+
+    #define ARG(i) ARG_PREFIX##i
+
     #ifdef ACNT1
-        #define TEST_START int test(int a0)
+        #define TEST_START \
+        int main(int argc, char** argv){        \
+            int ARG_PREFIX##0 = *((int*)(argv));    
+            
     #elif defined(ACNT2)
-        #define TEST_START int test(int a0, int a1)
+        #define TEST_START \
+        int main(int argc, char** argv){        \    
+            int ARG_PREFIX##0 = *((int*)(argv));    \
+            int ARG_PREFIX##1 = *((int*)(argv) + 1);
+
     #elif defined(ACNT3)
-        #define TEST_START int test(int a0, int a1, int a2)
+        #define TEST_START \
+        int main(int argc, char** argv){        \    
+            int ARG_PREFIX##0 = *((int*)(argv));    \
+            int ARG_PREFIX##1 = *((int*)(argv) + 1);\
+            int ARG_PREFIX##2 = *((int*)(argv) + 2);
+
     #elif defined(ACNT4)
-        #define TEST_START int test(int a0, int a1, int a2, int a3)
+        #define TEST_START \
+        int main(int argc, char** argv){        \    
+            int ARG_PREFIX##0 = *((int*)(argv));    \
+            int ARG_PREFIX##1 = *((int*)(argv) + 1);\
+            int ARG_PREFIX##2 = *((int*)(argv) + 2);\
+            int ARG_PREFIX##3 = *((int*)(argv) + 3);
     #endif
+    
 
-    #define TEST_END(retval) return retval;
-
-    #define ARG(i) a##i
 
 #endif
